446cb67afeea832a3cc3ca697736952d99f4f307
kabman1000 <elviskabutey69@gmail.com>
2021-02-22 04:21:37 +0000

    mapped url to appropriate views, created additional views and added some templates

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), #specifies path to its view 
+    path('', views.index, name='index'), #specifies path to its index view 
+    path('books/', views.BookListView.as_view(), name='books'), #specifies path to its Books views
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
